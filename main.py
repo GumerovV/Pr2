d5ece8f795f34f39713cdccba912d2ce1cdf86b5
Admin <gumerov.vlad20034@gmail.com>
2022-04-04 11:55:11 +0700

    add file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def less():
-    month = 0
-    marks = 0
+def gaps():
+    month = marks = sumdays = 0
     days = 2
-    sumdays = 0
     while(marks<=70 and month<9):
         marks+= days*2+3
         sumdays+=days
@@ -11,5 +9,7 @@ def less():
     print("Количество уходов в загул: ", month-1)
     print("Количество прогулов: ", sumdays-days+1)
 
-print("Чтобы не быть очисленным, Василий может допустить следующее количество")
-less()
+def main():
+    print("Чтобы не быть очисленным, Василий может допустить следующее количество")
+    gaps()
+main()
